832aaa9564a32f2fa060aeddbac6ff7bffecd057
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-02-18 20:38:20 +0800

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <main className="w-screen h-screen bg-orange-200">
+            <h1 className="w-12 h-12 bg-red-100"></h1>
+        </main>
+    );
+}
+
+export default App;
