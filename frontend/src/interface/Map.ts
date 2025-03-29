ea273ba208042f3cbe0238dff101d9e041abd340
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-03-29 19:40:24 +0800

    change the wall from line to vertices

--- frontend/src/interface/Map.ts
+++ frontend/src/interface/Map.ts
@@ -1,34 +1,22 @@
-export interface Dot {
-    x: number;
-    y: number;
-}
+import { Vector2D } from "./Vector2D";
 
 export interface Wall {
-    start: Dot;
-    end: Dot;
+    corners: Vector2D[]
 }
 
 export class MapSystem {
-    dots: Dot[] = [];
     walls: Wall[] = [];
 
-    draw(ctx: CanvasRenderingContext2D) {
-        // Draw dots
-        ctx.fillStyle = "black";
-        this.dots.forEach(({ x, y }) => {
-            ctx.beginPath();
-            ctx.arc(x, y, 3, 0, Math.PI * 2);
-            ctx.fill();
-        });
-
-        // Draw walls
-        ctx.strokeStyle = "blue";
-        ctx.lineWidth = 2;
-        this.walls.forEach(({ start, end }) => {
-            ctx.beginPath();
-            ctx.moveTo(start.x, start.y);
-            ctx.lineTo(end.x, end.y);
-            ctx.stroke();
-        });
-    }
+    // draw(ctx: CanvasRenderingContext2D) {
+    //     // Draw walls
+    //     ctx.strokeStyle = "blue";
+    //     ctx.lineWidth = 2;
+    //     console.log(this.walls)
+    //     this.walls.forEach(({ start, end }) => {
+    //         ctx.beginPath();
+    //         ctx.moveTo(start.x, start.y);
+    //         ctx.lineTo(end.x, end.y);
+    //         ctx.stroke();
+    //     });
+    // }
 }
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -7,121 +7,122 @@ import { drawTank } from "../interface/Tank";
 import { drawProjectile } from "../interface/Projectile";
 import { interpolateEntities } from "../utils/interpolation";
 import GameState from "../interface/GameState";
+import { Wall } from "../interface/Map";
 
 const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:4000");
 
 function Game() {
-    const params = useParams<{ roomId: string }>();
-    const canvasRef = useRef<HTMLCanvasElement | null>(null);
-    const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
+  const params = useParams<{ roomId: string }>();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = useRef<CanvasRenderingContext2D | null>(null);
 
-    const [gameState, setGameState] = useState<GameState>({
-        id: "",
-        map: {},
-        players: {},
-        projectiles: {},
+  const [gameState, setGameState] = useState<GameState>({
+    id: "",
+    map: {},
+    players: {},
+    projectiles: {},
+  });
+  const previousState = useRef<GameState>(gameState);
+  const lastUpdateTime = useRef<number>(performance.now());
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    ctxRef.current = canvasRef.current.getContext("2d");
+  }, []);
+
+  useEffect(() => {
+    const gameListener = new GameListener();
+
+    const checkSocketReady = setInterval(() => {
+      if (socket.id) {
+        console.log("Socket ID ready:", socket.id);
+        setupControls(gameListener, socket, params.roomId, socket.id);
+        clearInterval(checkSocketReady);
+      }
+    }, 100); // Check every 100ms until socket.id is available
+
+    socket.emit("joinRoom", params.roomId, (room: any) => {
+      console.log("Joined room:", room);
     });
-    const previousState = useRef<GameState>(gameState);
-    const lastUpdateTime = useRef<number>(performance.now());
 
-    useEffect(() => {
-        if (!canvasRef.current) return;
-        ctxRef.current = canvasRef.current.getContext("2d");
-    }, []);
+    socket.on("gameState", (newGameState: GameState) => {
+      lastUpdateTime.current = performance.now();
+      previousState.current = gameState;
+      setGameState(newGameState);
+    });
 
-    useEffect(() => {
-        const gameListener = new GameListener();
+    gameListener.start();
 
-        const checkSocketReady = setInterval(() => {
-            if (socket.id) {
-                console.log("Socket ID ready:", socket.id);
-                setupControls(gameListener, socket, params.roomId, socket.id);
-                clearInterval(checkSocketReady);
-            }
-        }, 100); // Check every 100ms until socket.id is available
+    return () => {
+      socket.off("gameState");
+      gameListener.stop();
+    };
+  }, [params.roomId]);
 
-        socket.emit("joinRoom", params.roomId, (room: any) => {
-            console.log("Joined room:", room);
-        });
+  useEffect(() => {
+    let animationFrameId: number;
 
-        socket.on("gameState", (newGameState: GameState) => {
-            lastUpdateTime.current = performance.now();
-            previousState.current = gameState;
-            setGameState(newGameState);
-        });
+    const animate = () => {
+      if (!ctxRef.current) return;
+      const ctx = ctxRef.current;
+      ctx.clearRect(0, 0, 1280, 720);
 
-        gameListener.start();
+      const now = performance.now();
+      const deltaTime = Math.min((now - lastUpdateTime.current) / 16.67, 1); // Normalize for 60 FPS
 
-        return () => {
-            socket.off("gameState");
-            gameListener.stop();
-        };
-    }, [params.roomId]);
+      const interpolatedPlayers = interpolateEntities(
+        gameState.players,
+        previousState.current.players,
+        deltaTime
+      );
+      const interpolatedProjectiles = interpolateEntities(
+        gameState.projectiles,
+        previousState.current.projectiles,
+        deltaTime
+      );
 
-    useEffect(() => {
-        let animationFrameId: number;
+      Object.values(interpolatedPlayers).forEach((player) =>
+        drawTank(player, ctx)
+      );
 
-        const animate = () => {
-            if (!ctxRef.current) return;
-            const ctx = ctxRef.current;
-            ctx.clearRect(0, 0, 1280, 720);
+      Object.values(interpolatedProjectiles).forEach((projectile) =>
+        drawProjectile(projectile, ctx)
+      );
 
-            const now = performance.now();
-            const deltaTime = Math.min(
-                (now - lastUpdateTime.current) / 16.67,
-                1
-            ); // Normalize for 60 FPS
+      drawMap(gameState);
 
-            const interpolatedPlayers = interpolateEntities(
-                gameState.players,
-                previousState.current.players,
-                deltaTime
-            );
-            const interpolatedProjectiles = interpolateEntities(
-                gameState.projectiles,
-                previousState.current.projectiles,
-                deltaTime
-            );
-
-            Object.values(interpolatedPlayers).forEach((player) =>
-                drawTank(player, ctx)
-            );
-
-            Object.values(interpolatedProjectiles).forEach((projectile) =>
-                drawProjectile(projectile, ctx)
-            );
-
-            drawMap(gameState);
-
-            animationFrameId = requestAnimationFrame(animate);
-        };
-
-        animationFrameId = requestAnimationFrame(animate);
-        return () => cancelAnimationFrame(animationFrameId);
-    }, [gameState]);
-
-    const drawMap = (gameState: GameState) => {
-        if (!ctxRef.current) return;
-        const ctx = ctxRef.current;
-
-        gameState.map.walls.forEach((wall: any) => {
-            ctx.beginPath();
-            ctx.moveTo(wall.start.x, wall.start.y);
-            ctx.lineTo(wall.end.x, wall.end.y);
-            ctx.stroke();
-        });
+      animationFrameId = requestAnimationFrame(animate);
     };
 
-    return (
-        <div>
-            <canvas
-                ref={canvasRef}
-                width={1000}
-                height={600}
-                className="bg-blue-100"
-            />
-        </div>
-    );
+    animationFrameId = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [gameState]);
+
+  const drawMap = (gameState: GameState) => {
+    if (!ctxRef.current) return;
+    const ctx = ctxRef.current;
+
+    console.log(gameState.map.walls);
+    gameState.map.walls.forEach((wall: Wall) => {
+      ctx.beginPath();
+      ctx.moveTo(wall.corners[0].x, wall.corners[0].y);
+      wall.corners.forEach((corner) => {
+        ctx.lineTo(corner.x, corner.y);
+      });
+      ctx.fill();
+    });
+  };
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        width={1000}
+        height={600}
+        className="bg-blue-100"
+      />
+    </div>
+  );
 }
 
 export default Game;
