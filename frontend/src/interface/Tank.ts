afe9cee29619a74ece963ba0befc533a97180d72
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-03-30 23:22:53 +0800

    vercel debug

--- frontend/src/interface/Tank.ts
+++ frontend/src/interface/Tank.ts
@@ -5,6 +5,8 @@ export function drawTank(player: Entity, ctx: CanvasRenderingContext2D) {
     ctx.beginPath();
 
     player.hitbox.vertices.forEach(({ x: relX, y: relY }, index) => {
+        if(!player.position.x) return
+
         if (index === 0) {
             ctx.moveTo(player.position.x + relX, player.position.y + relY);
         } else {
