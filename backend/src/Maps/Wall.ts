f44220d2840307b68158cc0d79e6265280e0f10f
TechnoDev <74283450+TechnoX0@users.noreply.github.com>
2025-04-29 09:41:00 +0800

    added more functions

--- backend/src/Maps/Wall.ts
+++ backend/src/Maps/Wall.ts
@@ -2,35 +2,54 @@ import Collision from "../Utils/Collision";
 import { CollisionType } from "../Utils/Enums";
 import Vector2D from "../Utils/Vector2D";
 
+interface WallEdge {
+    start: Vector2D;
+    end: Vector2D;
+}
+
 class Wall {
     corners: Vector2D[];
     collision: Collision;
+    edges: WallEdge[];
 
     constructor(start: Vector2D, end: Vector2D, thickness: number) {
         this.corners = this.calculateCorners(start, end, thickness);
         this.collision = new Collision(CollisionType.polygon, this.corners[0], this.corners)
+        this.edges = this.calculateEdges();
     }
 
     private calculateCorners(start: Vector2D, end: Vector2D, thickness: number): Vector2D[] {
-        const dx = end.x - start.x;
-        const dy = end.y - start.y;
-        const length = Math.sqrt(dx * dx + dy * dy);
-        
-        // Normalize direction
-        const nx = dx / length;
-        const ny = dy / length;
-        
-        // Perpendicular vector
-        const perpX = -ny * (thickness / 2);
-        const perpY = nx * (thickness / 2);
-        
+        const direction = end.subtract(start).normalized;
+        const perp = direction.perpendicular().multiply(thickness / 2);
+
         return [
-            new Vector2D(start.x - perpX, start.y - perpY),
-            new Vector2D(start.x + perpX, start.y + perpY),
-            new Vector2D(end.x + perpX, end.y + perpY),
-            new Vector2D(end.x - perpX, end.y - perpY),
-            new Vector2D(start.x - perpX, start.y - perpY) // Closing the shape
+            start.subtract(perp),
+            start.add(perp),
+            end.add(perp),
+            end.subtract(perp),
+            start.subtract(perp) // Closing the shape
         ];
+    }
+
+    private calculateEdges(): WallEdge[] {
+        const edges: WallEdge[] = [];
+        for (let i = 0; i < this.corners.length - 1; i++) {
+            edges.push({
+                start: this.corners[i],
+                end: this.corners[i + 1]
+            });
+        }
+        return edges;
+    }
+
+    get getNormal(): Vector2D {
+        const start = this.corners[0];
+        const end = this.corners[1];
+
+        const direction = end.subtract(start).normalized;
+        const normal = direction.perpendicular().normalized;
+
+        return normal;
     }
 }
 
